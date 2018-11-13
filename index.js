2f49af5e543776380daff6a08d418fb1081b0f4f
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-11-13 19:04:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.login(process.env.TOKEN);
 bot.on("ready", () => {
   console.log("Je suis prêt")
   
-bot.user.setActivity("[/help] I CPES®『🚫』3 servs", {type:"STREAMING"});    
+bot.user.setActivity("[/help] I SP®『🚫』3 servs", {type:"STREAMING"});    
 });
 
 bot.on("guildMemberAdd", member => {
