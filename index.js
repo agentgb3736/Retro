db38dd385bb516d0360a84e7312a9357fbd298bf
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-11-25 09:46:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.login(process.env.TOKEN);
 bot.on("ready", () => {
   console.log("Je suis prêt")
   
-bot.user.setActivity("[/help] I SP®『🚫』22 servs", {type:"WATCHING"});
+bot.user.setActivity("[/help] I SP®『🚫』24 servs", {type:"WATCHING"});
 bot.user.setStatus('dnd')
 });
 
