0813ca70dac06de36361786fbcb3f4c07967643b
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-11-27 07:26:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.login(process.env.TOKEN);
 bot.on("ready", () => {
   console.log("Je suis prêt")
   
-bot.user.setActivity("[/help] I SP®『🚫』26 servs.", {type:"WATCHING"});
+bot.user.setActivity("[/help] I SP®『🚫』27 servs.", {type:"WATCHING"});
 bot.user.setStatus('dnd')
 });
 
