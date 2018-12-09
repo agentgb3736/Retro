b69b5eccd81dac6557e3312118aaff865a17a8db
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-12-09 22:32:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.login(process.env.TOKEN);
 bot.on("ready", () => {
   console.log("Je suis prêt")
   
-bot.user.setActivity("[/help] 40s. I SP®『🚫』Anti-Raid 🚨", {type:"STREAMING"});    
+bot.user.setActivity("[/help] 44s. I SP®『🚫』Anti-Raid 🚨", {type:"STREAMING"});    
 });
 
 bot.on("guildMemberAdd", member => {
