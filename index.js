84846c13a00303f9c2ad27c4f051cb25d8008446
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-12-14 19:17:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -264,7 +264,7 @@ var y = new Discord.RichEmbed()
 .addField("🚔 Message envoyé par "+message.author.username+"#"+message.author.discriminator, "🆔 "+message.author.id)
 message.channel.send("**Tous le staff sp à été alerté ! 🚨**")
 bot.channels.find("id", "522508136884731904").send(y)
-bot.channels.find("id", "522508136884731904").send(" <@&511106212587372544>")
+bot.channels.find("id", "522508136884731904").send(" <@&516633518609137688>")
 message.delete()
 }
 
