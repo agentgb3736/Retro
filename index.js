27c49c7fd53608730e18c1e687004a4612185c8b
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-12-15 15:03:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ if(message.content === prefix + "help") {
            .addField("__**Administration**__ - (2)", "``/ban`` ``/kick``") 
            .addField("__**Fun**__ - (3)", "``/say`` ``/vcs`` ``/8ball``") 
            .addField("__**Autres**__ - (2)", "``/info`` ``/invite``") 
-           .addField("__**Anti-Raid**__ - (3)", "``/rules`` ``/sp`` ``/report``")
+           .addField("__**Anti-Raid**__ - (4)", "``/rules`` ``/sp`` ``/report`` ``/rb``")
            .setFooter("© 2018 SecurityProtect V.3.1.0", bot.user.displayAvatarURL) 
            .setTimestamp()
       message.channel.send(help_embed)
