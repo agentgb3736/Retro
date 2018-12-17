3315ff37334ea756a16fc9281b988a80c08a38b8
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-12-17 19:32:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ bot.login(process.env.TOKEN);
 
 bot.on("ready", () => {
   console.log("Je suis prêt")
-  
-bot.user.setActivity("[/help] 71s. I SP®『🚫』Anti-Raid 🚨", {type:"WATCHING"});  
+setInterval(function(){
+
+ bot.user.setActivity('[/help] ' + bot.guilds.size + 's. | SP®『🚫』Anti-Raid 🚨', {type:"WATCHING"});
+}, 2000);  
 bot.user.setStatus('dnd')  
 });
 
