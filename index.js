79f20cbf249f550fd1a182693adb581d9ba47dc9
ClesiriusGaming <40736194+ClesiriusGaming@users.noreply.github.com>
2018-12-19 19:13:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bot.on('ready', () => {
     setInterval(changing_status, 1000);
   
     function changing_status() {
-      let status = ["Protèger" +client.guilds.size+" serveurs ", "/help /invite", "Crée par _ClesiriusPE _「🎄」", "Joyeux Noël「🎄」", "「👮」17 065  users protégés", "#Go80servs", "「💪」On compte sur vous"]
+      let status = ["Protèger" +bot.guilds.size+" serveurs ", "/help /invite", "Crée par _ClesiriusPE _「🎄」", "Joyeux Noël「🎄」", "「👮」17 065  users protégés", "#Go80servs", "「💪」On compte sur vous"]
       let random = status[Math.floor(Math.random() * status.length)]
       bot.user.setActivity(random)
   }
